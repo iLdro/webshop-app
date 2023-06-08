ec5d7823f1266108a5b66325ca4d3f2ee793154e
Julien Klein <44435573+iLdro@users.noreply.github.com>
2023-06-08 09:58:59 +0200

    connecting mongoDB

--- api/db.js
+++ api/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/webshoop-app', { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
