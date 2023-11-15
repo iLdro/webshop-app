b45173b81c340b58783ef7421d4efbf99cc1b10c
Valentin <valentin.labrune@gmail.com>
2023-11-15 10:47:49 +0100

    update cors to accept ports 8080 and 3000

--- api/index.js
+++ api/index.js
@@ -8,7 +8,11 @@ app.use(cors({
     origin: (origin, callback) => {
         // Check if the request origin is allowed
         // You can implement your own logic here to validate the origin
-        const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+        const allowedOrigins = [
+            'http://localhost:5173', 'http://127.0.0.1:5173', 
+            'http://localhost:3000', 'http://127.0.0.1:3000',
+            'http://localhost:8080', 'http://127.0.0.1:8080'
+        ];
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
